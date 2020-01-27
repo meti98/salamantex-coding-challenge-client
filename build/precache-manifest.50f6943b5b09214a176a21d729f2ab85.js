b38e34521c1d9ceb124442b25da7f516fd5009d8
Matthias Schaider <matthias@websteps.at>
2020-01-27 09:36:04 +0100

    added build folder

---
+++ build/precache-manifest.50f6943b5b09214a176a21d729f2ab85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c830eaa07be594699c8bd75fe8a75e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30be5def0e270f19f6ef",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae8426fb817e33418968",
+    "url": "/static/js/2.5426aad6.chunk.js"
+  },
+  {
+    "revision": "67080489def75028d866a9df60514f14",
+    "url": "/static/js/2.5426aad6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30be5def0e270f19f6ef",
+    "url": "/static/js/main.94d3befb.chunk.js"
+  },
+  {
+    "revision": "2425da6c2724064e6b3c",
+    "url": "/static/js/runtime-main.a6754b21.js"
+  }
+]);
